b93fbd7ce8b8606ae2a42fe3f1af29824586f192
Scott Chiang <scott.chiang1@gmail.com>
2013-07-04 23:03:49 -0700

    added parse login delegates

--- Swap/SWPLoginViewController.m
+++ Swap/SWPLoginViewController.m
@@ -26,7 +26,7 @@
 -(void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    if (![PFUser currentUser]) {
+//    if (![PFUser currentUser]) {
         PFLogInViewController *loginViewController = [[PFLogInViewController alloc] init];
         [loginViewController setDelegate:self];
         
@@ -36,7 +36,7 @@
         [loginViewController setSignUpController:signupViewController];
         
         [self presentViewController:loginViewController animated:YES completion:NULL];
-    }
+//    }
 }
 
 - (void)viewDidLoad
@@ -49,6 +49,36 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark Parse login  
+
+-(BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password
+{
+    if (username && password && username.length != 0 && password.length != 0) {
+        return YES;
+    }
+    
+    [[[UIAlertView alloc] initWithTitle:@"Missing Info" message:@"Please fill out all the information" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil] show];
+    
+    return NO;
+}
+
+-(void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user
+{
+//    [self dismissViewControllerAnimated:YES completion:NULL];
+    
+    //after user logs in, show the main controller
+    // use [self.navigationController pushViewController:controller ...]
+    
+}
+
+- (void)logInViewController:(PFLogInViewController *)logInController didFailToLogInWithError:(NSError *)error {
+    NSLog(@"Failed to log in...");
+}
+
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
