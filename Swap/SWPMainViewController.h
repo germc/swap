b5eb4a46e10a6f3257e557ca7b89d3fe86b03166
Scott Chiang <scott.chiang1@gmail.com>
2013-07-05 23:00:32 -0700

    move parse login and signup controller stuff to main viewcontroller class

---
+++ Swap/SWPMainViewController.h
@@ -0,0 +1,14 @@
+//
+//  SWPMainViewController.h
+//  Swap
+//
+//  Created by Scott Chiang on 7/5/13.
+//  Copyright (c) 2013 Scott Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface SWPMainViewController : UIViewController <PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+
+@end
