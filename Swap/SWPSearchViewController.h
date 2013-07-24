c77c8ff873caee6587a9cbea133b24ad07c0eec4
Scott Chiang <scott.chiang1@gmail.com>
2013-07-23 22:55:53 -0700

    added SWPSearchViewController

---
+++ Swap/SWPSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWPSearchViewController.h
+//  Swap
+//
+//  Created by Scott Chiang on 7/23/13.
+//  Copyright (c) 2013 Scott Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWPSearchViewController : UIViewController
+
+@end
